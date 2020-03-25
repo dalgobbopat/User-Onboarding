8200b9a86366073332c5fad47ec63f043c1e70d2
Patrick Dal Gobbo <juandalgobbo@Juans-Air.lan1>
2020-03-25 16:40:30 -0400

    starting

---
+++ magician/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1>Magic 101 🧙‍♂️</h1>
+      
+    </div>
+  );
+}
+
+export default App;
